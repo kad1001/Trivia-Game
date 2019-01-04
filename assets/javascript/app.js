e954170c3e8f1f8ab392a221ccb858545557c397
Kelly davis <kellyadavis7@gmail.com>
2019-01-03 21:42:55 -0500

    appearence

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+
+{buttons
+    "$ 1000" value = 1000, function onclick -> $grid -> easy science question + input box + countdown from 5 seconds
+    "$ 2000" value=2000, function onclick -> $grid -> med science question + input box + countdown
+    "$ 3000" value=3000, function onclick -> $grid -> hard science question + input + countdown
+}
+
+{buttons
+    "$ 1000" value = 1000, function onclick -> $grid -> easy computer question + input box + countdown from 5 seconds
+    "$ 2000" value = 2000, function onclick -> $grid -> med computer question + input box + countdown
+    "$ 3000" value = 3000, function onclick -> $grid -> hard computer question + input + countdown
+}
+{buttons
+    "$ 1000" value = 1000,function onclick -> $grid -> easy Entertainment question + input box + countdown from 5 seconds
+    "$ 2000" value = 2000,function onclick -> $grid -> med Entertainment question + input box + countdown
+    "$ 3000" value = 3000, function onclick -> $grid -> hard Entertainment question + input + countdown
+}
+
+*if user does not click button in 5 seconds, will alert ("you ran out of time") and location.reload
+*if user enters correct response, alert("you are correct! you have collected" + money)
+*if user enters correct response, record money value into "money" array, which stores the sum of the values of each correct guess
