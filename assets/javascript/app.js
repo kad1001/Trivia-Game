979ec64436b202f8d416812a00ea4017ef07a129
Kelly davis <kellyadavis7@gmail.com>
2019-01-14 15:13:16 -0500

    need score function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,3 @@
-// $("#s1000").click(function() {
-//     $(".container").replaceWith($(".questions"));
-// })
-
-// second: m
-
-
-
-
-
-
-// {buttons
-//     "$ 1000" value = 1000, function onclick -> $grid -> easy science question + input box + countdown from 5 seconds
-//     "$ 2000" value=2000, function onclick -> $grid -> med science question + input box + countdown
-//     "$ 3000" value=3000, function onclick -> $grid -> hard science question + input + countdown
-// }
-
-// {buttons
-//     "$ 1000" value = 1000, function onclick -> $grid -> easy computer question + input box + countdown from 5 seconds
-//     "$ 2000" value = 2000, function onclick -> $grid -> med computer question + input box + countdown
-//     "$ 3000" value = 3000, function onclick -> $grid -> hard computer question + input + countdown
-// }
-// {buttons
-//     "$ 1000" value = 1000,function onclick -> $grid -> easy Entertainment question + input box + countdown from 5 seconds
-//     "$ 2000" value = 2000,function onclick -> $grid -> med Entertainment question + input box + countdown
-//     "$ 3000" value = 3000, function onclick -> $grid -> hard Entertainment question + input + countdown
-// }
 
 // *if user does not click button in 5 seconds, will alert ("you ran out of time") and location.reload
 // *if user enters correct response, alert("you are correct! you have collected" + money)
