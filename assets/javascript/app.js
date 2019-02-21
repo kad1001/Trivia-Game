f5a6c0bb7a870ecaa2488537f21c53b48f05aea6
Kelly davis <kellyadavis7@gmail.com>
2019-02-21 02:13:42 -0500

    question buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,54 @@
+var time;
+moneyWon = [];
+moneyLost = [];
 
- var time;
- moneyWon = [];
- moneyLost = [];
 
-
-var th1 =  $("<th>");
+var th1 = $("<th>");
 
 var th2 = $("<th>");
 
-var th3 =  $("<th>");
-
+var th3 = $("<th>");
 
 var headers = [th1, th2, th3];
+
+var body = $("#tBody");
+// var easyOne = $(".easyOne")
+
+// var columnOneDivs = [$("")]
+
+// storing category numbers
+// this way we can access them in the array and call a new ajax request on them
+// this can be done by matching the col of the button from getValue() to the designated category number in all
+var all = [];
+
+var buttonsArr = [];
+
+
+
+// onclick function to get column
+
 
 
 // gets random category number to plug in for url
 function getRndInt(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 };
 
 
-    // constructor for Categroy
- function Category(Name, question, correct_answer, incorrect_answers, value) {
-     this.Name = Name;
-     this.question = question;
-     this.correct_answer = correct_answer,
-     this.incorrect_answers = incorrect_answers,
-     this.value = value;
+// constructor for Categroy
+function Category(Name, question, correct_answer, incorrect_answers, difficulty) {
+    this.Name = Name,
+    this.question = question,
+    this.correct_answer = correct_answer,
+        this.incorrect_answers = incorrect_answers,
+        this.difficulty = difficulty
 
-    // method that console logs the answers
-    this.printAnswers = function(){
-        console.log("Answers: " + this.correct_answer + this.incorrect_answers);
-    }
-
- };
+};
 
 
 
 //  gets ajax response, can potentially be called for each column and append data but who the fuck knows these days
 function newCat() {
-
     // gets always global (random) catNumb the random cat number
     // can take anywhere from 9 to 32 cats in 1 sitting hopefully
     var queryURL = ("https://opentdb.com/api.php?amount=3&category=" + catNumb + "&type=multiple");
@@ -47,21 +56,34 @@ function newCat() {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
-        // console.log(response);
+    }).then(function (response) {
+
         catfunction1(response);
+
     });
 };
 
-    // appends new category to each thing u know it does the thing
-for (var v in headers) {
+
+
+// for each header (3), perform ajax and push category numbs to all
+for (var a = 0; a < headers.length; a++) {
+    var queries = [];
+
+    var divBtn = $("<div>");
+
+
     var catNumb = getRndInt(9, 32);
 
     newCat();
-}
+
+};
 
 
-function catfunction1(data){
+
+
+
+// gets data
+function catfunction1(data) {
 
     // saves the data response into a new Category
     var d = data.results[0];
@@ -71,103 +93,43 @@ function catfunction1(data){
     var triviaI = d.incorrect_answers;
     var triviaValue = d.difficulty;
 
-    console.log(d);
 
-    var catty = new Category(triviaName, triviaQ, triviaC, triviaI, triviaValue);
-    console.log(catty);
+    var category = new Category(triviaName, triviaQ, triviaC, triviaI, triviaValue);
+    console.log(category);
 
-    var catsyo = $("<td>").text(catty.Name);
-
-
-    $("#sciRow").append(catsyo);
-
-    $(".easy").on("click", function (){
-        console.log(catty);
-        // if (event.target === $(".easy")) {
-        // console.log(d);
-        // }
-    })
+    var header = $("<td>").text(category.Name);
 
 
-}
+    $("#sciRow").append(header);
 
-
-
-
-// // each of the headers in da categories array - the element is the literal th. maybe 
-// headers.forEach(function(element){
-//     var rand = getRndInt(9, 32);
-
-//     var queryURL = ("https://opentdb.com/api.php?amount=3&category=" + rand + "&type=multiple");
-
-// $.ajax({
-// url: queryURL,
-// method: "GET"
-// // passes the data to createButtons
-// }).then(function(response){
-
-//     console.log(response);
-  
-
-
-//     // returns a category name purrr
-//     var kitty = response.results[0].category;
-//     console.log(kitty);
-//     showQ1(response);
-//     showQ2(response);
-
-//     // add to fam
-//     $(element).append(kitty);
-//     $(element).attr("name", kitty);
-
-
-//     var cat1 = new 
-
-//     // let's store each of these kittehs in add it as an attr to the element, like a name
-//     console.log($(element).attr("name"));
-//     // return title;
-
-//     // $(".easy").on("click", function(){
-//     //     console.log("easy");
-//     //     });
-//  });
-    
-// });
-
-// function showQ1(data){
-// console.log(data);
-// var question = data.results[0].question;
-// console.log(question);
-
-// }
-
-// function showQ2(data){
-//    console.log(data);
-// }
-
-
-
-// window.onload = function(){
-
-//     $("#sciRow").append(th1, th2, th3);
-
-
-
-// var catNumb = getRndInt(9, 32);
-
-// console.log("Category number: ", catNumb);
-// $.ajax({
-// url: queryURL,
-// method: "GET"
-// // passes the data to createButtons
-// }).then(function(response){
-// //   createButtons(response);
-//     console.log(response);
- 
-//  });
-// };
+    all.push(category);
+    console.log(all);
 
     
+    var question = $("<button>").text("$1000");
+    console.log(category.question);
+
+    for (let w = 0; w < all.length; w++ ) {
+
+        var button = $(question);
+        button.click(function() {
+            
+            $(question).text(category.question)
+
+
+        });
+        button.appendTo("#rowOne");
+        
+    }
+
+
+
+
+};
+
+
+
+
 // // this will shuffle the answers
 // function shuffle(array) {
 //     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -184,53 +146,27 @@ function catfunction1(data){
 //     return array;
 //     };
 
-// function newTrivia(subject, difficulty){
-//     var queryURL = ("https://opentdb.com/api.php?amount=1&category=" + subject + difficulty + "&type=multiple");
-//     $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//     // passes the data to createButtons
-//   }).then(function(response){
-//       createButtons(response);
-//      });
-// };
-
-//      // data displayed on buttons on screen
-//      var createButtons = function(data){
-         
-//         var results = data.results[0];
-//         // json
-//         var question = results.question;
-//         var difficulty = results.difficulty;
-//         console.log(difficulty);
-
-//         var correct_answer = results.correct_answer;
-//         var incorrect_answer1 = results.incorrect_answers[0];
-//         var incorrect_answer2 = results.incorrect_answers[1];
-//         var incorrect_answer3 = results.incorrect_answers[2];
-
-//     // initial money count
-//     var money = 0;
-
-
-//         // hides  quiz layout
-//        $("#quiz").hide();
-
 
 //     //    creates new div for dynamic info
 //     var buttonDiv = $("<div>");
-//     // new header for question
-//     var header = $("<h2>");
+
 //     //    displays question
 //     $(header).text(question);
+
 //     // assign whitefont class for css
 //     $(header).attr("class", "whitefont");
 //     $(header).attr("class", "font-effect-3d-float");
+
 //     // append header to buttondiv
 //     $(buttonDiv).append(header);
 
 
+// ========================
+// buttons
+// =========================
+
 //     // puts each incorrect answer in an array that will give it a button
+
 //         incbuttonsArray = [];
 //         incbuttonsArray.push(incorrect_answer1, incorrect_answer2, incorrect_answer3);
 //         // buttonsArray = shuffle(buttonsArray);
@@ -243,6 +179,7 @@ function catfunction1(data){
 //             $(buttonInc).attr("id", "incorrect");
 //             $(buttonInc).attr("class", "font-effect-3d-float");
 //         };
+
 
 //         // create seperate btn for correct answer
 //         var correctB = $("<button>").append(correct_answer);
@@ -308,7 +245,7 @@ function catfunction1(data){
 //             // money -= 1000;
 //             moneyLost.push(-1000);
 //             // update money display
- 
+
 //         };
 //         if (difficulty == "hard"){
 //             // money -= 3000;
@@ -317,18 +254,17 @@ function catfunction1(data){
 //         };
 //         $("#moneyLost").text(moneyLost);
 //     };
-   
-   
+
+//    $("#quiz").attr("Id", "quiz");
 
 //     // parent div event listener
-//     document.getElementById("practiceDiv").addEventListener("click", function(e){
-    
-//         // clears timer
-//         clearInterval(counter);
+// document.getElementById("easy2").addEventListener("click", function(e){
+
 
 //     // alerts the user of whether they were correct or not
 //             // e.srcElement was the clicked element
-//         if (e.srcElement.id == correct_answer) {
+// if (e.srcElement == col1easy) {
+
 //             console.log("Correct answer!");
 //             $("#winLose").text("Correct! Money Added!")
 //             addMoney();
@@ -337,13 +273,13 @@ function catfunction1(data){
 //             // show quiz layout
 //             $("#quiz").show();
 
-//             };
+// };
 //         if (e.srcElement.id == "incorrect") {
 //             console.log("Sorry, wrong answer");
 //             $("#winLose").text("NOPE! Money Lost")
 //             subtractMoney();
 //             $(buttonDiv).empty();
 //             $("#quiz").show();
-//         };
-//     });
+// });
+// });
 // };
