992a36e19ce2806bd2305ea50917fc7f3531f313
Kelly davis <kellyadavis7@gmail.com>
2019-02-22 22:22:54 -0500

    removed Bulma, now there is a small black border around the quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,6 @@ var th3 = $("<th>");
 var headers = [th1, th2, th3];
 
 
-
 var body = $("#tBody");
 
 
@@ -63,7 +62,7 @@ function Category(Name, question, correct_answer, incorrect_answers, difficulty)
 
 
 
-//  gets ajax response, can potentially be called for each column and append data but who the fuck knows these days
+//  gets ajax response
 function newCat() {
     // gets always global (random) catNumb the random cat number
     // can take anywhere from 9 to 32 cats in 1 sitting hopefully
@@ -74,12 +73,7 @@ function newCat() {
         method: "GET"
     }).then(function (response) {
 
-        // console.log(response.results[0]);
         catfunction1(response);
-
-        catfunction2(response);
-
-        // new Category(response.)
 
     });
 };
@@ -92,19 +86,17 @@ for (var a = 0; a < headers.length; a++) {
 
     var divBtn = $("<div>");
 
-
     var catNumb = getRndInt(9, 32);
 
     newCat();
-    // newCat();
 
 };
 
 
 
 
-
 // gets data
+
 function catfunction1(data) {
 
     // saves the data response into a new Category
@@ -121,7 +113,6 @@ function catfunction1(data) {
 
     var header = $("<td>").text(category.Name);
 
-
     $("#sciRow").append(header);
 
     all.push(category);
@@ -130,7 +121,7 @@ function catfunction1(data) {
 
     var question = $("<button>").text("$1000");
 
-    $(question).attr("class", "button is-large");
+    $(question).attr("class", "moneyButtons");
 
 
     console.log(category.question);
@@ -139,8 +130,6 @@ function catfunction1(data) {
     answersArr.push(category.incorrect_answers, category.correct_answer);
     var concatArr = answersArr[0].concat(answersArr[1]);
 
-    console.log(concatArr);
-
     shuffle(concatArr);
 
     console.log(concatArr);
@@ -148,31 +137,36 @@ function catfunction1(data) {
 
     // please append these to the response div
     function please() {
+        // ========================
+        // buttons
+        // =========================
+
         var more = document.getElementById("responseDiv");
         for (var i = 0; i < concatArr.length; i++) {
             var butt = document.createElement("button");
             butt.innerHTML = concatArr[i];
-            $(butt).attr("class", "answer");
+            $(butt).attr("class", "answerBtn");
             more.appendChild(butt);
         }
 
 
-        var butt = $(".answer");
+        var butt = $(".answerBtn");
         console.log(butt);
+        $(butt).attr("class", "answerQ");
+        // $(butt).attr("class", "answerBtn");
 
-        $(butt).click(function(){
+        $(butt).click(function () {
 
-   
             var clickedAnswer = $(this).html();
 
-    
             console.log(category);
             console.log(clickedAnswer);
 
             if (clickedAnswer === category.correct_answer) {
-                console.log("You're correct!")
-            }
+                console.log("You're correct!");
+                $("#results").text("Correct!");
 
+            } 
             else {
                 console.log("Sorry, that's not correct.")
             }
@@ -182,107 +176,25 @@ function catfunction1(data) {
         });
 
 
-
-    
     };
 
 
-
-
     var button = $(question);
+    button.click(function() {
 
-
-    button.click(function () {
-
-        // $(question).text(category.question);
         $("#displayQ").text(category.question);
 
         please();
 
-    })
-
+    });
 
     button.appendTo("#rowOne");
 
 
-
-
 };
 
 
 
-
-function catfunction2(data) {
-    console.log(data.results[0]);
-};
-
-
-//     //    creates new div for dynamic info
-//     var buttonDiv = $("<div>");
-
-//     //    displays question
-//     $(header).text(question);
-
-//     // assign whitefont class for css
-//     $(header).attr("class", "whitefont");
-//     $(header).attr("class", "font-effect-3d-float");
-
-//     // append header to buttondiv
-//     $(buttonDiv).append(header);
-
-
-// ========================
-// buttons
-// =========================
-
-//     // puts each incorrect answer in an array that will give it a button
-
-//         incbuttonsArray = [];
-//         incbuttonsArray.push(incorrect_answer1, incorrect_answer2, incorrect_answer3);
-//         // buttonsArray = shuffle(buttonsArray);
-//         // give each answer a button
-//         for (let v = 0; v < incbuttonsArray.length; v++){
-//             var buttonInc = $("<button>").append(incbuttonsArray[v]);
-//             // append each button to buttons div
-//             $(buttonDiv).append(buttonInc);
-//             // assign buttoninc an id attribute of its element
-//             $(buttonInc).attr("id", "incorrect");
-//             $(buttonInc).attr("class", "font-effect-3d-float");
-//         };
-
-
-//         // create seperate btn for correct answer
-//         var correctB = $("<button>").append(correct_answer);
-//         // assign this an id to track
-//         $(correctB).attr("id", correct_answer);
-//         $(correctB).attr("class", "font-effect-3d-float");
-
-//         // make array to shuffle order of questions
-//         newArray = [buttonInc, correctB];
-//         newArray = shuffle(newArray);
-
-//         // add whitefont class to newArray for css
-//         $(newArray).attr("class", "whitefont");
-
-
-//         // add this to the button div
-//         $(buttonDiv).append(newArray);
-
-//     // append buttonDiv to practiceDiv
-//     $("#practiceDiv").append(buttonDiv);
-//     // display timer
-//     // hooks to html div
-//     // display = document.querySelector("time");
-//     // startTimer(10, display);
-
-//     // alerts the user "time's up" after 10 seconds
-
-
-
-//     // function for answer selection
-
-//     // ends the timeout
-//     // updates money
 //     function addMoney(){
 //         if (difficulty == "medium"){
 //             money += 2000;
@@ -324,10 +236,5 @@ function catfunction2(data) {
 //         };
 //         $("#moneyLost").text(moneyLost);
 //     };
-
-
-
-//     // parent div event listener
-// document.getElementById("easy2").addEventListener("click", function(e){
 
 
